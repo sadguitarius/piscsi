9a2249f318d3455224f2a279dbbeca4d1a1dedbc
sadguitarius <justbecuz@gmail.com>
2023-10-30 15:45:36 -0700

    sg super custom setup

--- cpp/controllers/scsi_controller.h
+++ cpp/controllers/scsi_controller.h
@@ -25,7 +25,7 @@ class PrimaryDevice;
 class ScsiController : public AbstractController
 {
 	// For timing adjustments
-	static const unsigned int MIN_EXEC_TIME = 50;
+	static const unsigned int MIN_EXEC_TIME = 400;
 
 	// Transfer period factor (limited to 50 x 4 = 200ns)
 	static const int MAX_SYNC_PERIOD = 50;
